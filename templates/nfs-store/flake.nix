270f726bf73a1d01d2e8ca28284224d37b8f44aa
Olivier Richard <olivier.richard@imag.fr>
2026-05-17 10:31:13 +0200

    add g5k-nfs-store and templates/nfs-store

---
+++ templates/nfs-store/flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Default NixOS image for Grid'5000 Testbed";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixos-g5k-image = {
+      url = "github:oar-team/nixos-g5k-image";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-g5k-image,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      lib = pkgs.lib;
+
+      nixosConfig = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          nixos-g5k-image.nixosModules.g5k-nfs-store.nix
+          ./configuration.nix
+        ];
+      };
+    in
+
+    {
+      packages.${system} = rec {
+        #g5k-nfs-store-info = nixosConfig.config.system.build.g5k-nfs-store-info;
+        g5k-nfs-store = nixosConfig.config.system.build.toplevel;
+        default = g5k-nfs-store;
+      };
+
+      formatter.${system} = pkgs.writeShellScriptBin "formatter" ''
+        set -eoux pipefail
+        shopt -s globstar
+        ${lib.getExe pkgs.deno} fmt README.md
+        ${lib.getExe pkgs.nixpkgs-fmt} .
+        ${lib.getExe pkgs.just} --fmt --unstable
+      '';
+    };
+}
